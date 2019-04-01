bb076db8530ba685f9315139fd3a51e8ed02567d
E-man <emmanuel.donohue@gmail.com>
2019-03-31 18:47:05 -0700

    cleaned up comments, added sample metadata to home page

--- static/js/app.js
+++ static/js/app.js
@@ -19,22 +19,18 @@ function buildMetadata(sample) {
 
     // Use `Object.entries` to add each key and value pair to the panel
     Object.entries(metadata).forEach((key, value) => { 
-      sampMeta.append(metadata).text(`${key}: ${value}`);
+      sampMeta.append("div").text(`${key}: ${value}`);
 
-    });       //what goes after .append(  ????
+    });       
   });
 }
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
 
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var url = "/samples" + "/" + sample;        //should I add '+ sample' to url ln35?
+  var url = "/samples" + "/" + sample;        
 
   d3.json(url).then(function(data1){
     console.log("buildCharts function works");
@@ -42,12 +38,11 @@ function buildCharts(sample) {
 
     // @TODO: Build a Bubble Chart using the sample data
 
-    //++Does our url endpoint return data in a json object w/ the keys we want? 
-      //++If so, no need for data manip, can just pass directly in the plot method.
 
-    var trace = [{                    //+we get the trace values per the readme
-      x: data1.otu_ids,               //+what is ' markers' mode?
-      y: data1.sample_values,         //what values after size/colors?
+
+    var trace = [{                    //we get the trace values per the readme
+      x: data1.otu_ids,               
+      y: data1.sample_values,         
       text: data1.otu_labels,
       mode: 'markers',
       marker: {
@@ -60,19 +55,15 @@ function buildCharts(sample) {
     var layout = {
       title: "Bubble Chart"
     };
-    //use .newPlot do the new plot can overwrite the old one
+    //use .newPlot for the plot, since they can overwrite the old one
     Plotly.newPlot("bubble", trace, layout);   
 
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-                            //++per readme sample_values as values for pie chart
-                            // otu_ids as labels
+    // @TODO: Build a Pie Chart               
     var layout1 = {
       title: "Pie Chart"
-    };                                          //++I think I have Pie Chart CORRECT..
-    var datax = [{                           //++..EXCEPT for the hovertext
+    };                                          
+    var datax = [{                           
       values: data1.sample_values.slice(0,11),
       labels: data1.otu_ids.slice(0, 11),
       type: "pie"
@@ -115,10 +106,4 @@ function optionChanged(newSample) {
 // Initialize the dashboard
 init();
 
-//QUESTIONS ?!?!?!?!?!
-//should I keep '/<sample>' sample at the end of the url paths /samples and /metadata?
-//what goes after .append(  around ln20 ?
-//what values after size/colors in the trace?
-//add or delte semicolon on ln26, 82, 104, 111? 
-// "/samples" or "/samples/" ? "/metadata" or "/metadata/" ? or "/samples" + "/" + sample
-//ln 76/77 changed datax.sample_values to data1.sample_values
+
